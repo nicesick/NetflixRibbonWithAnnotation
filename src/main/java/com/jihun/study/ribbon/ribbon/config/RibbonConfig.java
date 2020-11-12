5a53a6499e5e618f6ea5a120b6dfff4048ee896b
Aha <mistel1327@naver.com>
2020-11-12 15:53:03 +0900

    Edit RibbonConfig for customizing IPing and IRule

--- src/main/java/com/jihun/study/ribbon/ribbon/config/RibbonConfig.java
+++ src/main/java/com/jihun/study/ribbon/ribbon/config/RibbonConfig.java
@@ -1,7 +1,32 @@
 package com.jihun.study.ribbon.ribbon.config;
 
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
 public class RibbonConfig {
     /*
      * You can customize your ribbon loadBalancer in here
      */
+
+    private IClientConfig clientConfig;
+
+    @Autowired
+    public RibbonConfig(IClientConfig clientConfig) {
+        this.clientConfig = clientConfig;
+    }
+
+    @Bean
+    public IPing getPing(IClientConfig config) {
+        return new PingUrl();
+    }
+
+    @Bean
+    public IRule getRule(IClientConfig config) {
+        return new AvailabilityFilteringRule();
+    }
 }
